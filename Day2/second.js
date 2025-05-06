625f0e11bbe16f0d9b556ec8849d0cda97d59992
Rajkoli143 <your.email@example.com>
2025-05-06 13:01:14 +0530

    Java-workshop day-2

---
+++ Day2/second.js
@@ -0,0 +1,117 @@
+// function tempconverter(celsius) {
+//   const fahrenheit = (celsius * 9) / 5 + 32;
+//   return fahrenheit;
+// }
+// let temp = prompt("Enter the temperature:");
+// console.log(`${temp}°C is ${tempconverter(temp)}°F`);
+
+// console.log("Hello")
+// let str = "This is the \"string\""
+// console.log(str)
+// console.log(str.length);
+// console.log(str.charAt(0));
+// console.log(str.indexOf("is"));
+// console.log(str.lastIndexOf("is"));
+// console.log(str.slice(0, -4));
+// console.log(str.substring(0, -4));
+// console.log(str.replace("is", "was"))
+// console.log(str.at(0));
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log()
+
+
+// 
+// function str() {
+//     let string1 = prompt("Enter first string:");
+//     let string2 = prompt("Enter second string:");
+//     if(string1==string2){
+//         console.log('True')
+//     }
+//     else{
+//         console.log('False')
+//     }
+// }
+// str();
+
+// let str = "this"
+// for(let s of str){
+//     console.log(s)
+// }
+
+
+// let str1= prompt("Enter the string:")
+// console.log(str1.slice(1));
+
+
+
+
+
+
+// create a function that takes an array and a string as arguments and returns the index of the string. 
+// function findIndex(list, word) {
+//     return list.indexOf(word);
+//   }
+  
+//   let words = ["The", "This", "These"];
+//   let search = "The";
+//   let result = findIndex(words, search);
+  
+//   console.log("Index of", search, "is:", result);
+  
+
+// Padding in Js
+// str= "5";
+// let padded = str.padEnd(5,"*")
+// let padded1= str.padStart(5,"*")
+// console.log(padded)
+// console.log(padded1)
+
+
+// {
+//     str="This is the classroom"
+// }
+// console.log(str)
+
+
+// In Js function don't worry about what number of argument or which type of parameter u are passing
+// function demo(num1){
+//     let str = "This is the classroom"
+//     console.log(num1)
+// }
+// demo(4,7)
+// // Output: 4
+
+
+// Passing n number of parameter with one arguments
+// function demo1(...num){
+//     let str = "This is the classroom"
+//     console.log(num)
+// }
+// demo1(4,4,6,5,2,9,6,7,2)
+
+// deflaut parameter
+// function demo1(fun=4,...num){
+//     let str = "This is the classroom"
+//     console.log(num)
+//     console.log(fun)
+// }
+// demo1(4,4,6,5,2,9,6,7,2)
+
+
+// minimalic Function:
+// const func =() => console.log("HEllO")
+// func()
+
+
+
+arr= [1,2,3,4,5,6,7,8,9,10]
+console.log(typeof arr)
+
+
+
+
+
+
+
+
